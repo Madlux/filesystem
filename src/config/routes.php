61d9f223b269ff837cc8fab901d784b6049ae236
Madlux <nikita.baray@gmail.com>
2016-01-26 10:40:38 +0400

    1

--- src/config/routes.php
+++ src/config/routes.php
@@ -1,7 +1,5 @@
 <?php
 
-Route::any('/users', 'Packages\Users\Controllers\UsersController@indexAction');
-Route::any('/users/files', 'Packages\Users\Controllers\FilesController@indexAction');
-Route::any('/users/files/save', 'Packages\Users\Controllers\FilesController@saveFileAction');
-Route::any('/users/files/delete', 'Packages\Users\Controllers\FilesController@deleteFileAction');
-Route::any('/users/verify', 'Packages\Users\Controllers\VerifyController@verifyAction');
+Route::any('/files', 'Madlux\Filesystem\Controllers\FilesController@indexAction');
+Route::any('/files/save', 'Madlux\Filesystem\Controllers\FilesController@saveFileAction');
+Route::any('/files/delete', 'Madlux\Filesystem\Controllers\FilesController@deleteFileAction');
